7e7b58578f09a11a22990fab160c582c9d315ddb
Anna <anna.tiala@vaimo.com>
2022-12-28 14:11:06 +0200

    added logo to navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -11,15 +11,27 @@ export default function Navigation({ allPages }: Props) {
 
   const pages = allPages;
 
+  const frontPage = allPages.length > 0 ? allPages[1] : undefined;
+
+  console.log(frontPage);
+
   return (
     <nav className="shadow-md fixed top-0 w-full">
       {router.isFallback ? (
         <p>Loading…</p>
       ) : (
-        <div className="container mx-auto pt-4 pb-4 px-6">
+        <div className="container flex mx-auto pt-4 pb-4 px-6">
+          <img
+            className="nav-logo pr-4"
+            src="https://images.ctfassets.net/hef5a6s5axrs/2xHFmDBAHOHhEb09JF9oli/38c52caec01752cf1e6044c3b5cc4241/dog-logo-sircle.png"
+          ></img>
           {pages.length > 0 &&
             pages.map((page) => (
-              <a className="pr-4" aria-label={page.pageName} href={""}>
+              <a
+                className="pr-4"
+                aria-label={page.pageName}
+                href={page?.pageName}
+              >
                 {page?.pageName}
               </a>
             ))}
