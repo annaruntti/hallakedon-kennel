420e85a2cbc30b346f87986e07d14beefd1abcff
Anna <anna.tiala@vaimo.com>
2023-04-24 13:38:00 +0300

    navigation style changes and add mobile menu

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -41,6 +41,13 @@ export function pagesToMenuItems(pages: Page[]): MenuItem[] {
 }
 
 export default function Navigation({ menuItems }: Props) {
+  const [isMobileMenuOpen, setOpenMobileMenu] = useState(false);
+  const [isMobileMenuLevel2Open, setOpenMobileMenuLevel2] = useState(false);
+
+  const openMobileMenu = () => setOpenMobileMenu(!isMobileMenuOpen);
+  const openMobileMenuLevel2 = () =>
+    setOpenMobileMenuLevel2(!isMobileMenuLevel2Open);
+
   const [isSubMenuOpen, setOpenSubMenu] = useState(false);
   const [isSubMenuLevel2Open, setOpenSubLevel2Menu] = useState(false);
 
@@ -55,11 +62,90 @@ export default function Navigation({ menuItems }: Props) {
   return (
     <nav className="shadow-md fixed top-0 w-full">
       <div className="block md:hidden container flex mx-auto pt-4 pb-4 px-6">
-        <img className="inline w-10 mr-4" src="/logo.png" />
-        <button className="inline-flex ml-auto">
+        <Link href="/">
+          <img className="inline w-10 mr-4" src="/logo.png" />
+        </Link>
+        <button className="inline-flex ml-auto" onClick={openMobileMenu}>
           <Bars3Icon className="pt-2 h-8 w-8 mr-2" />
           <span className="pt-2 font-bold">Valikko</span>
         </button>
+      </div>
+      <div
+        className={
+          isMobileMenuOpen ? "mobile-menu-open-level1" : "mobile-menu-close"
+        }
+      >
+        <ul className="mobile-menu-list">
+          <li>
+            <Link href="/">Etusivu</Link>
+          </li>
+          {menuItems.length > 0 &&
+            menuItems.map((itemLevel1, indexLevel1) => (
+              <li key={indexLevel1}>
+                {itemLevel1.subItems.length > 0 ? (
+                  <button className="inline-flex" onClick={openSubMenu}>
+                    {itemLevel1.title}{" "}
+                    {isSubMenuOpen ? (
+                      <ChevronUpIcon className="h-4 w-4 pt-1 ml-2" />
+                    ) : (
+                      <ChevronDownIcon className="h-4 w-4 pt-1 ml-2" />
+                    )}
+                  </button>
+                ) : (
+                  <Link href={`/${itemLevel1.slug}`}>{itemLevel1.title}</Link>
+                )}
+
+                {itemLevel1.subItems.length > 0 && (
+                  <ul
+                    className={
+                      isSubMenuOpen ? "submenu-open-level1" : "submenu-close"
+                    }
+                  >
+                    {itemLevel1.subItems.map((itemLevel2, indexLevel2) => (
+                      <li key={indexLevel2}>
+                        {itemLevel2.subItems.length > 0 ? (
+                          <button
+                            className="inline-flex"
+                            onClick={openSubMenuLevel2}
+                          >
+                            {itemLevel2.title}
+                            {isSubMenuLevel2Open ? (
+                              <ChevronUpIcon className="h-4 w-4 pt-1 ml-2" />
+                            ) : (
+                              <ChevronDownIcon className="h-4 w-4 pt-1 ml-2" />
+                            )}
+                          </button>
+                        ) : (
+                          <Link href={`/${itemLevel2.slug}`}>
+                            {itemLevel2.title}
+                          </Link>
+                        )}
+                        {itemLevel2.subItems.length > 0 && (
+                          <ul
+                            className={
+                              isSubMenuLevel2Open
+                                ? "submenu-open-level2"
+                                : "submenu-close"
+                            }
+                          >
+                            {itemLevel2.subItems.map(
+                              (itemLevel3, indexLevel3) => (
+                                <li key={indexLevel3}>
+                                  <Link href={`/${itemLevel3.slug}`}>
+                                    {itemLevel3.title}
+                                  </Link>
+                                </li>
+                              )
+                            )}
+                          </ul>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+        </ul>
       </div>
       <div className="hidden md:block container flex mx-auto pt-4 pb-4 px-6">
         <ul className="list-none flex flex-row gap-4 items-center">
